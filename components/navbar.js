9c695be8a13bb8a9a3c2468cffa0bed53585a18b
Iwona Jeleń <iwoj345@gmail.com>
2021-01-26 16:34:24 +0100

    Fix navbar on phone screen

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import { signIn, signOut, useSession } from 'next-auth/client';
-import { Button, Nav, Navbar, Form, FormControl, Row, Col, Container } from 'react-bootstrap';
+import { Button, Nav, NavDropdown, Navbar, Form, FormControl, Row, Col, Container } from 'react-bootstrap';
 import { useMediaQuery } from 'react-responsive';
 import { Search } from 'react-bootstrap-icons';
 import { useRouter } from 'next/router';
@@ -23,21 +23,33 @@ const HeaderNavbar = () => {
     }
 
     return(<>
-        <Navbar bg="dark" variant="dark" className="d-flex justify-content-between">
-            <Nav>
-            {session && <>
-            <Link href="/recipes"><a className="nav-link">Recipes</a></Link>
-            <Link href="/new-recipe"><a className="nav-link">Create</a></Link></>}
-            </Nav>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="d-flex justify-content-between">
+            {session && session.user.name && isTabletOrMobile && <>
+                <Button variant="secondary" onClick={signOut}>Sign out</Button>
+            </>}
             {session && !isTabletOrMobile && <>
+            <Nav>
+            <Link href="/recipes"><a className="nav-link">Recipes</a></Link>
+            <Link href="/new-recipe"><a className="nav-link">Create</a></Link>
+            <Link href="/shopping-list"><a className="nav-link">Shopping list</a></Link>
+            </Nav>
             <Form inline>
                 <FormControl type="text" placeholder="Search" onChange={(e) => setSearchQuery(e.target.value)} />
                 <Link href={hrefObj()}>
                     <Button variant="success" className="ml-sm-3">Search</Button>
                 </Link>
             </Form></>}
-            {!session && <Button onClick={signIn}>Sign in</Button>}
-            {session && session.user.name && <>
+            {session && isTabletOrMobile && <>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ml-auto">
+                    <Link href="/recipes"><a className="nav-link text-center">Recipes</a></Link>
+                    <Link href="/new-recipe"><a className="nav-link text-center">Create</a></Link>
+                    <Link href="/shopping-list"><a className="nav-link text-center">Shopping list</a></Link>
+                </Nav>
+            </Navbar.Collapse></>}
+            {!session && <Button className="ml-auto" onClick={signIn}>Sign in</Button>}
+            {session && session.user.name && !isTabletOrMobile && <>
                 <Navbar.Text className="d-none d-sm-block d-md-none" style={{margin: 'auto 20px'}}>
                     {session.user.name}
                 </Navbar.Text>
